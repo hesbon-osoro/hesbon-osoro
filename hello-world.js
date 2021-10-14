93596a7fe855313e32fbf9674d1b9b44f74c5f45
wazimu <hesbonosoro1@gmail.com>
2021-10-15 00:34:42 +0300

    Added some introductory files

---
+++ hello-world.js
@@ -0,0 +1,3 @@
+console.log('Hello World')
+console.log('Wab developa!')
+console.log('All the Web developer journey starts by logging out through the console')
---
+++ hello-world.py
@@ -0,0 +1,2 @@
+print('Hello World')
+print('This is where all the developer journey begins')
