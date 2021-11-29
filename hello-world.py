b1334c6a01efd1c81f93219ad4835e539b069f11
wazimuhb <hesbonosoro1@gmail.com>
2021-11-29 23:50:16 +0300

    praising python

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,4 @@
 print('Hello World')
 print('This is where all the developer journey begins')
+print('This is the language that I use in competitive programming challenges')
+print('I love how it is simple to learn and how it is fun to code and solve complex challenges')
