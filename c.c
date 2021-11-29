dd6218bbd388bb3977fde827c4ee0e9653e84445
wazimuhb <hesbonosoro1@gmail.com>
2021-11-29 23:45:53 +0300

    C language

---
+++ c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<stdbool.h>
+#include<limits.h>
+#include<float.h>
+#include<ctype.h>
+
+int main(int argc, char **argv){
+    printf("Hello World!\nI have some experience in this legendary language.\nOh my bad! I didn't use the imported Libraries!");
+}
