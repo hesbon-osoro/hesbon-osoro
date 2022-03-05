00fc3f328d889b17bf261377ccdd97893c22dc3e
wazimuhb <hesbonosoro1@gmail.com>
2022-03-05 10:33:04 +0300

    js skill

---
+++ js.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+console.log('Wab developa!');
+console.log(
+  'All the Web developer journey starts by logging out through the console',
+  'This is the langauage I have prepared myself for the future',
+  'It is the language to put food before the table.'
+);
