075664a8c006b928a21fd9bee40d9018802045b2
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-04 00:23:29 +0300

    prettier formatting

--- ts.ts
+++ ts.ts
@@ -1,8 +1,8 @@
 export interface ITs {
-	name: string;
+  name: string;
 }
 
 export const ts = <T>(name: string) => {
-	console.log(`${name}, this is the future of JavaScript`);
+  console.log(`${name}, this is the future of JavaScript`);
 };
 ts<ITs>('TypeScript');
